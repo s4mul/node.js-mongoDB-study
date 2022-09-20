809c224ae811e3670eace2f2371fae3d7a89d847
Sunwoo Kim <k_sunoo@naver.com>
2022-09-21 00:44:09 +0900

    11/14

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;//몇글자인지
 
 const userSchema = mongoose.Schema({
     name:{
@@ -30,6 +31,27 @@ const userSchema = mongoose.Schema({
     tokenExp:{
         type: Number
     }
+});
+
+userSchema.pre('save', function(next){
+
+    var user = this;
+    //비밀번호 암호화시킨다.
+    //bcrypr를 사용함.
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            //salt를 만들고 saltround자릿수의
+            if(err) return next(err);
+    
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                // Store hash in your password DB.
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
+    }
+    
 });
 
 const User = mongoose.model('User',userSchema);
